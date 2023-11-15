72077d113b51f128a7db551ddedb1dc1a7970794
tk <90978402+totolkat@users.noreply.github.com>
2023-11-15 15:49:14 +0000

    Actualizar katosay.js
    More clear on what to do if you did not put a text and added comments.

--- katosay.js
+++ katosay.js
@@ -1,10 +1,12 @@
 import colors from "colors";
-import process from "process";
+import process from "process"; 
 
 
-const text = process.argv.slice(2).join(" ")
+const text = process.argv.slice(2).join(" ") // getting argument provided and joining the string 
+
+// checking if text is empty
 if (text == "" || text == "") {
-    console.log("You put nothing in the text.");
+    console.log("node katosay.js [text]. You didn't put anything in the text.");
 } else {
 console.log(` 
          < ${colors.bold(text)} >
